9aea733f2cec0d463b626d1cc8f48c185b0ef12a
Simon Le Marchant <simon@marchantweb.com>
2024-01-18 09:22:56 -0500

    chore: Renamed custom.css as landing.css

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './styles/vars.css'
-import './styles/custom.css'
+import './styles/landing.css'
 import AsideSponsors from './components/AsideSponsors.vue'
 import SvgImage from './components/SvgImage.vue'
 
